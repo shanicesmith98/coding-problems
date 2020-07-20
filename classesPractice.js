b11fefe9a42b9b0d1ae7467e685640ba4170d06f
shanice <shanicesmith.work@hotmail.com>
2020-07-19 20:02:27 -0400

    classes practice in js

---
+++ classesPractice.js
@@ -0,0 +1,43 @@
+// we can import and export code to reference and reuse in external files!
+export class Character {
+	constructor(age, job, fandom) { //
+    	this.age = age;
+      	this.job = job;
+      	this.fandom = fandom;
+    }
+  
+  	doSomething(age) {
+ 		this.age >= 18 ? console.log(`You're of age .. Be a hero and save the world!`) : console.log(`You're only ${this.age} .. Have a safe day at school!`);
+    } 
+  
+  	get metaData() {
+  		return `Age: ${this.age}, Job: ${this.job}, Fandom: ${this.fandom}`;
+  	}
+  
+}
+
+export class Hero extends Character { // inherits everything from Character class
+	constructor(age, job, fandom, weapon) {
+    	super(age, job, fandom, weapon);
+      	this.weapon = weapon;
+    }
+}
+
+/***** Page Break *****/
+
+import { Character } from './character.mjs';
+import { Hero } from './character.mjs';
+
+let peterParker = new Character(17, 'Student', 'Marvel'); // creates instance of Character class
+let jasonTodd = new Character(20, 'Vigilante', 'DC');
+
+console.log(peterParker.metaData);
+console.log(jasonTodd.metaData); // displays properties or metadata of instance
+
+peterParker.doSomething();
+jasonTodd.doSomething();
+
+let tonyStark = new Hero(48, 'Engineer', 'Marvel', 'Repulsor Rays');
+
+console.log(tonyStark.metaData);
+tonyStark.doSomething();
