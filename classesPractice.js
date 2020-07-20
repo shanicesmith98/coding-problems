2278485c017394abe39f71c4d5fc5e30c7346bef
shanice <shanicesmith.work@hotmail.com>
2020-07-19 20:44:52 -0400

    added comments

--- classesPractice.js
+++ classesPractice.js
@@ -1,16 +1,16 @@
 // we can import and export code to reference and reuse in external files!
 export class Character {
-	constructor(age, job, fandom) { //
-    	this.age = age;
-      	this.job = job;
-      	this.fandom = fandom;
-    }
+	constructor(age, job, fandom) { // is the "blueprint" for creating instances of Character class
+		this.age = age;
+		this.job = job;
+		this.fandom = fandom;
+   	 }
   
   	doSomething(age) {
  		this.age >= 18 ? console.log(`You're of age .. Be a hero and save the world!`) : console.log(`You're only ${this.age} .. Have a safe day at school!`);
-    } 
+    	} 
   
-  	get metaData() {
+  	get metaData() { // function to get data
   		return `Age: ${this.age}, Job: ${this.job}, Fandom: ${this.fandom}`;
   	}
   
@@ -18,9 +18,9 @@ export class Character {
 
 export class Hero extends Character { // inherits everything from Character class
 	constructor(age, job, fandom, weapon) {
-    	super(age, job, fandom, weapon);
-      	this.weapon = weapon;
-    }
+    		super(age, job, fandom, weapon); // calls constructor properties from Character class (Parent class)
+      		this.weapon = weapon; // new property inside Hero class
+    	}
 }
 
 /***** Page Break *****/
